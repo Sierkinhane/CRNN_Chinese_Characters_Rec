fd959b91c28c198bbcf3cf8a74efb62f1b02c99a
Louis <sierkinhane@gmail.com>
2019-08-04 21:32:19 +0800

    Update compute_stds_means.py

--- compute_stds_means.py
+++ compute_stds_means.py
@@ -4,4 +4,4 @@ import utils
 txt_path = "H:/DL-DATASET/360M/train.txt"
 img_prefix = "H:/DL-DATASET/360M/images"
 stds, means = utils.compute_std_mean(txt_path, img_prefix, NUM=1000)
-print("stds = "stds, "means = "means)
+print("stds = ", stds , "means = ", means)
